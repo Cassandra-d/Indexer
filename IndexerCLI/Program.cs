371eaeea4c817636b198f05ee31c087c2f15b5ed
CassandraD <2866070+Cassandra-d@users.noreply.github.com>
2019-08-04 23:28:03 +0300

    working lucene

--- IndexerCLI/Program.cs
+++ IndexerCLI/Program.cs
@@ -1,6 +1,7 @@
 using IndexerLib;
 using IndexerLib.FilesLookup;
 using IndexerLib.LASParser;
+using IndexerLucen;
 using System;
 using System.Threading.Tasks;
 using System.Collections.Generic;
@@ -12,8 +13,12 @@ namespace IndexerCLI
     {
         static async Task Main(string[] args)
         {
-            Console.Write("Directory:");
+            Console.Write("Directory or press Enter to use assembly folder:");
             var directory = Console.ReadLine();
+
+            if (directory == string.Empty)
+                directory = Environment.CurrentDirectory;
+
             var validator = new DirectoryPathValidator();
             var error = validator.Validate(directory);
 
@@ -37,13 +42,20 @@ namespace IndexerCLI
             }
 
             Console.WriteLine($"Found {filePaths.Length} files");
+
+            if (filePaths.Length == 0)
+            {
+                Console.WriteLine("Have nothing to do, halting");
+                Environment.Exit(0);
+            }
+
             Console.WriteLine("Parsing files. Wait a sec...");
 
             LASFilesParser parser = new LASFilesParser(Environment.ProcessorCount);
             IEnumerable<LASFileData> structuredData = Enumerable.Empty<LASFileData>();
             try
             {
-                structuredData =  await parser.Parse(filePaths);
+                structuredData = await parser.Parse(filePaths);
             }
             catch (Exception ex)
             {
@@ -52,10 +64,49 @@ namespace IndexerCLI
 
             Console.WriteLine("Building index. Wait a sec...");
 
+            var indexer = new Indexer();
+            Func<LASFileData, IEnumerable<string[]>> fieldsSelector =
+                x => x.Sections.SelectMany(s => s.Lines.Select(l => new[] { l.Description, l.Mnemonic }));
+            var d1 = structuredData.SelectMany(x => fieldsSelector(x).SelectMany(d => d.Select(z => (x.FilePath, z))));
 
+            try
+            {
+                indexer.Build(d1);
+            }
+            catch (Exception ex)
+            {
+                LogErrorAndDie(ex.Message);
+            }
 
-            Console.ReadKey();
+            Console.WriteLine("Index built successfully.");
+            Console.WriteLine("Just type field names and hit enter, and I will show results.");
+            Console.WriteLine("For example, type 'LTYP' or 'LOG TYPE'");
+
+            while (true)
+            {
+                Console.WriteLine("---------------");
+                var term = Console.ReadLine();
+
+                if (IsExit(term))
+                    Environment.Exit(0);
+
+                var res = indexer.Query(term);
+
+                foreach (var item in res)
+                {
+                    Console.WriteLine($"{item.field} - {item.filePath}");
+                }
+            }
         }
+
+        private static bool IsExit(string term)
+        {
+            return string.Compare(term, "q", ignoreCase: true) == 0 ||
+                   string.Compare(term, "quit", ignoreCase: true) == 0 ||
+                   string.Compare(term, "exit", ignoreCase: true) == 0;
+
+        }
+
         static void LogErrorAndDie(string errorMsg)
         {
             Console.WriteLine(errorMsg);
---
+++ IndexerLucen/Indexer.cs
@@ -0,0 +1,58 @@
+﻿using Lucene.Net.Store;
+using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Index;
+using Lucene.Net.Util;
+using Lucene.Net.Analysis;
+using Lucene.Net.Documents;
+using System.Collections.Generic;
+using Lucene.Net.Search;
+using Lucene.Net.QueryParsers;
+using System.Linq;
+
+namespace IndexerLucen
+{
+    public class Indexer
+    {
+        private Directory directory;
+        private readonly string PATH_FIELD = "path";
+        private readonly string FIELD_NAME_FIELD = "fieldName";
+
+        public Indexer()
+        {
+            directory = new RAMDirectory();
+        }
+
+        public void Build(IEnumerable<(string filePath, string field)> data)
+        {
+            Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_30);
+            IndexWriter writer = new IndexWriter(directory, analyzer, true, IndexWriter.MaxFieldLength.UNLIMITED);
+
+            foreach (var item in data)
+            {
+                Document doc = new Document();
+                doc.Add(new Field(PATH_FIELD, item.filePath, Field.Store.YES, Field.Index.NO));
+                doc.Add(new Field(FIELD_NAME_FIELD, item.field, Field.Store.YES, Field.Index.ANALYZED));
+
+                writer.AddDocument(doc);
+            }
+
+            writer.Dispose();
+        }
+
+        public IEnumerable<(string filePath, string field)> Query(string qText)
+        {
+            Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_30);
+            QueryParser parser = new QueryParser(Version.LUCENE_30, FIELD_NAME_FIELD, analyzer);
+            Query query = parser.Parse(qText);
+            IndexSearcher isearcher = new IndexSearcher(directory);
+            TopDocs hits = isearcher.Search(query, null, 1000);
+
+            foreach (var item in hits.ScoreDocs)
+            {
+                var doc = isearcher.Doc(item.Doc);
+                var res = (doc.Get(PATH_FIELD), doc.Get(FIELD_NAME_FIELD));
+                yield return res;
+            }
+        }
+    }
+}
